da6ca2b1d40c15e4c13b6fe30d43ec17f30586a2
hexq <hnldjia520@gmail.com>
2018-06-25 13:49:53 +0800

    delete test code

--- src/main/java/com/hexq/nms/test/Test.java
+++ src/main/java/com/hexq/nms/test/Test.java
@@ -5,14 +5,6 @@ package com.hexq.nms.test;
  * @date 2018/6/5 17:51
  */
 public class Test {
-    public static void main(String[] args) {
-        System.out.println(factorial(20L));
-    }
+    public static void main(String[] args) {}
 
-    public static long factorial(long i) {
-        if (i <= 0) {
-            return 1;
-        }
-        return i * factorial(i - 1);
-    }
 }
